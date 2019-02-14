9b62ab4e40d4749e1c83d7b26015599459fc1f1a
David Mogbeyi <mogbeyidavid@gmail.com>
2019-02-14 23:45:37 +0100

    Added pair solution

---
+++ decagon-labs/pair.js
@@ -0,0 +1,9 @@
+function pair(array) {
+  let result = new Map();
+  for (let index = 0; index < array.length; index++) {
+    result.set(array[index][0], array[index][array[index].length - 1]);
+  }
+  return result;
+}
+
+console.log(pair(['me', 'you', 'us', 'them']));
