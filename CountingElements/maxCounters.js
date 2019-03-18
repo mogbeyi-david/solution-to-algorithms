ffc9a8734b2f3d854b125e38e155d4590cbc0d0e
David Mogbeyi <mogbeyidavid@gmail.com>
2019-03-18 22:25:25 +0100

    Added solution for max counters

--- CountingElements/maxCounters.js
+++ CountingElements/maxCounters.js
@@ -1,22 +1,33 @@
-function solution(numberOfCounters, arrayOfOperations) {
+function solution(number, array) {
   let counters = [];
-  for (let i = 0; i < numberOfCounters; i++) {
+  for (let i = 0; i < number; i++) { //Build counters array
     counters.push(0);
   }
 
-  for (let index = 0; index < arrayOfOperations.length; index++) {
-    let operation = arrayOfOperations[index];
-    if (operation <= numberOfCounters) {
-      counters[operation - 1]++;
-    } else {
-      let maximumCounter = Math.max(...counters);
-      counters = counters.map((value) => {
-        value = maximumCounter;
-        return value;
-      });
+  for (let index = 0; index < array.length; index++) {
+    if (array[index] <= number) {
+      counters = increase(counters, array[index] - 1);
+    }
+
+    if (array[index] > number) {
+      counters = maxCounter(counters)
     }
   }
+
   return counters;
 }
 
-solution(5, [3, 6]);
+console.log(solution(5, [3, 4, 4, 6, 1, 4, 4]));
+
+function increase(array, index) {
+  array[index]++;
+  return array;
+}
+
+function maxCounter(array) {
+  const maximum = Math.max(...array);
+  for (let index = 0; index < array.length; index++) {
+    array[index] = maximum;
+  }
+  return array;
+}
