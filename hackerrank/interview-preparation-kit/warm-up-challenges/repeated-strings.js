197e3755137e7a12596e479dfce5aa2b1c5b63ae
David Mogbeyi <mogbeyidavid@gmail.com>
2020-01-24 18:32:42 +0100

    Added solution for repeated strings

---
+++ hackerrank/interview-preparation-kit/warm-up-challenges/repeated-strings.js
@@ -0,0 +1,18 @@
+function repeatedString(s, n) {
+    let c = 0,
+        ca = 0,
+        r = n % s.length;
+
+    for (let i = s.length; i-- > 0;) {
+        if (s.charAt(i) === 'a') {
+            ++c;
+            if (i < r) {
+                ++ca;
+            }
+        }
+    }
+
+    return ((n - r) / s.length * c) + ca
+}
+
+console.log(repeatedString("aba", 10));
